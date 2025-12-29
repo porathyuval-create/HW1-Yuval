700d9f1a40c69a8bc2e16104dfa79edb769e9bdb
porathyuval-create <porathyuval@gmail.com>
2025-12-29 17:12:23 +0200

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,7 +1,67 @@
-public class Bank {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bank implements IBank {
+
+    private List<IAccount> accounts;
 
     public Bank() {
-
+        accounts = new ArrayList<>();
     }
 
+    @Override
+    public void OpenAccount(IAccount account) {
+        if (account != null) {
+            accounts.add(account);
+        }
+    }
+
+    @Override
+    public void CloseAccount(int accountNumber) {
+        IAccount toRemove = null;
+
+        for (IAccount account : accounts) {
+            if (account.GetAccountNumber() == accountNumber) {
+                toRemove = account;
+                break;
+            }
+        }
+
+        if (toRemove == null) {
+            return;
+        }
+
+        if (toRemove.GetCurrentBalance() >= 0) {
+            accounts.remove(toRemove);
+        } else {
+            System.out.println("Account is not closed due to debt");
+        }
+    }
+
+    @Override
+    public List<IAccount> GetAllAccounts() {
+        return new ArrayList<>(accounts);
+    }
+
+    @Override
+    public List<IAccount> GetAllAccountsInDebt() {
+        List<IAccount> result = new ArrayList<>();
+        for (IAccount account : accounts) {
+            if (account.GetCurrentBalance() < 0) {
+                result.add(account);
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public List<IAccount> GetAllAccountsWithBalance(double amount) {
+        List<IAccount> result = new ArrayList<>();
+        for (IAccount account : accounts) {
+            if (account.GetCurrentBalance() > amount) {
+                result.add(account);
+            }
+        }
+        return result;
+    }
 }
