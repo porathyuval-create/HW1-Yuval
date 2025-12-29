d588a9e8527aa30a46fd8adb35693fce4fa8946c
porathyuval-create <porathyuval@gmail.com>
2025-12-29 17:17:54 +0200

    Update PremiumAccount.java

--- PremiumAccount.java
+++ PremiumAccount.java
@@ -1,7 +1,38 @@
-public class PremiumAccount {
+public class PremiumAccount implements IAccount {
+
+    private int accountNumber;
+    private double balance;
 
     public PremiumAccount(int accountNumber) {
-
+        this.accountNumber = accountNumber;
+        this.balance = 0;
     }
 
+    @Override
+    public void Deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        }
+    }
+
+    @Override
+    public double Withdraw(double amount) {
+        if (amount <= 0) {
+            return 0;
+        }
+
+        // Premium account has no credit limit
+        balance -= amount;
+        return amount;
+    }
+
+    @Override
+    public double GetCurrentBalance() {
+        return balance;
+    }
+
+    @Override
+    public int GetAccountNumber() {
+        return accountNumber;
+    }
 }
